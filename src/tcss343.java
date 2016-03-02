8228be7806649d3262ee10250f3ceaf82969d0a2
Bethany Eastman <bethanyeastman@gmail.com>
2016-03-01 16:01:15 -0800

    array now working for test file

--- src/tcss343.java
+++ src/tcss343.java
@@ -4,91 +4,102 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Program finds a solution that computes the cheapest sequence of rentals taking you from post
- * 1 all the way down to post n.
+ * Program finds a solution that computes the cheapest sequence of rentals
+ * taking you from post 1 all the way down to post n.
  * 
  * @author Antonio Alvillar, Gabriel Houle, Bethany Eastman
  * @version 03/01/2016
  */
 public class tcss343 {
-	
+
 	/**
-	 * Reads in rental costs and finds the minimum costs. 
+	 * Reads in rental costs and finds the minimum costs.
 	 * 
-	 * @param args - the text file of rental costs.
+	 * @param args
+	 *            - the text file of rental costs.
 	 */
 	public static void main(String[] args) {
 		Scanner input = null;
-		
+		Scanner input2 = null;
+
 		try {
 			input = new Scanner(new File(args[0]));
+			input2 = new Scanner(new File(args[0]));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		int numCols = 0;
 		while (input.hasNextLine()) {
-			while (input.hasNextInt()) {
-				System.out.print(input.nextInt() + ", ");
-			}
-			System.out.println();
-			while (input.hasNext() && !input.hasNextInt()){
-				System.out.print(input.next() + ", ");
+			input.nextLine();
+			numCols++;
+		}
+		
+		int[][] rentals = new int[numCols][numCols];
+		
+		for (int row = 0; row < numCols; row++) {
+			for (int col = 0; col < numCols; col++) {
+				if (input2.hasNextInt()) {
+					rentals[row][col] = input2.nextInt();
+				} else {
+					input2.next();
+					rentals[row][col] = Integer.MAX_VALUE;
+				}
 			}
 		}
 		
-		
-		int[][] rentals = { {0,2,3,7},
-							{Integer.MAX_VALUE,0,2,4},
-							{Integer.MAX_VALUE,Integer.MAX_VALUE,0,2},
-							{Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE, 0} };
 		System.out.println();
 		dynamic(rentals);
 	}
-	
+
 	/**
-	 * The brute force approach to solving the rentals problem. This 
-	 * solution runs in O(X).
+	 * The brute force approach to solving the rentals problem. This solution
+	 * runs in O(X).
 	 * 
-	 * @param rentals - array of rental costs.
+	 * @param rentals
+	 *            - array of rental costs.
 	 */
 	private static void bruteFore(int[][] rentals) {
-		
+
 	}
-	
+
 	/**
 	 * The Divide and Conquer approach to solving the rental problem. This
 	 * solution runs in O(X).
 	 * 
-	 * @param rentals - array of rental costs.
+	 * @param rentals
+	 *            - array of rental costs.
 	 */
 	private static void divideAndConquer(int[][] rentals) {
-		
+
 	}
-	
+
 	/**
 	 * The dynamic programming approach to solving the rentals problem. This
 	 * solution runs in O(x).
 	 * 
-	 * @param rentals - array of rental costs.
+	 * @param rentals
+	 *            - array of rental costs.
 	 */
 	private static void dynamic(int[][] rentals) {
 		for (int i = 0; i < rentals.length; i++) {
 			System.out.println(Arrays.toString(rentals[i]));
 		}
 		System.out.println();
-		
+
 		int n = 4;
 		int[] b = new int[n];
 		for (int i = 1; i < n; i++) {
 			b[i] = Integer.MAX_VALUE;
-			for (int j = i-1; j >= 0; j--) {
+			for (int j = i - 1; j >= 0; j--) {
 				b[i] = Math.min(rentals[j][i] + b[j], b[i]);
 				System.out.println(b[i]);
 			}
 		}
 		System.out.println();
-		
+
 		for (int i = 0; i < b.length; i++) {
 			System.out.print(b[i]);
-		}	
+		}
 	}
 }
