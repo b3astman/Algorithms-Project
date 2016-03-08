90bd94813789fe5d982f96cc7dcb0b3deb9ec708
Bethany Eastman <bethanyeastman@gmail.com>
2016-03-08 12:06:36 -0800

    random boolean matrix generated

--- src/challenge.java
+++ src/challenge.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 /*
  * TCSS343 Algos HW4 
  */
 
 
 public class challenge {
+	
+	private static Random rand = new Random();
+	private static boolean[][] field;
 
 	public static void main(String[] args) {
-		
+		createMatrix(4);
+		System.out.println(Arrays.deepToString(field));
+	}
+	
+	public static void createMatrix(int n) {
+		field = new boolean[n][n];
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+				field[row][col] = rand.nextBoolean();
+			}
+		}
 	}
 
 }
