5d8682a100ed7f6f63ff2c7d7a52973d8e4be2a2
ghoule636 <ghoule636@gmail.com>
2016-03-08 16:12:44 -0800

    Added brute force for field and stones.

--- src/challenge.java
+++ src/challenge.java
@@ -1,8 +1,6 @@
 import java.util.Arrays;
 import java.util.Random;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 /*
  * TCSS343 Algos HW4 
  */
@@ -14,8 +12,12 @@ public class challenge {
 	private static boolean[][] field;
 
 	public static void main(String[] args) {
-		createMatrix(4);
-		System.out.println(Arrays.deepToString(field));
+		createMatrix(6);
+		for (boolean [] theArr : field) {
+			System.out.println(Arrays.toString(theArr));
+		}
+		//System.out.println(Arrays.deepToString(field));
+		bruteForce();
 	}
 	
 	public static void createMatrix(int n) {
@@ -26,5 +28,46 @@ public class challenge {
 			}
 		}
 	}
-
+	
+	public static boolean checkTarget(int x, int y, int size) {
+		boolean result = true;
+		
+		if (y + size > field.length || x + size > field.length) {
+			result = false;
+		}
+		
+		for (int i = x; (i < size + x) && result; i++) {
+			for (int j = y; j < size + y; j++) {
+				if (field[i][j]) {
+					result = false;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public static void bruteForce() {
+		int maxI = 0;
+		int maxJ = 0;
+		int maxSize = 0;
+		boolean result = true;
+		
+		for (int i = 0; i <= field.length; i++) {
+			for (int j = 0; j <= field.length; j++) {
+				result = true;
+				for (int s = 0; (s <= field.length) && result; s++) {
+					if (checkTarget(i, j, s)) {
+						if (s > maxSize) {
+							maxI = i;
+							maxJ = j;
+							maxSize = s;
+						}
+					} else {
+						result = false;
+					}
+				}
+			}
+		}
+		System.out.println("I: " + maxI + " J: " + maxJ + " maxSize: " + maxSize);
+	}
 }
